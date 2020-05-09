86946ed6c478fad59013a7706e6bf12d93ef222d
Igor Thorik <habcerga1@gmail.com>
2020-05-09 15:10:16 +0300

    Features Added class GetDataAsync

--- AsyncMethods/Properties/AssemblyInfo.cs
+++ AsyncMethods/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotifyTaskCompletion")]
+[assembly: AssemblyTitle("AsyncMethods")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotifyTaskCompletion")]
+[assembly: AssemblyProduct("AsyncMethods")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
